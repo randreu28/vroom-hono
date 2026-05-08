a99666a7ba2839b52ca6893c56ba5837f3058465
Rubén Chiquin <randreu28@gmail.com>
2026-05-08 07:12:58 +0200

    feat: Adds .env example

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,7 +2,6 @@ import { Hono } from "hono";
 
 const health = new Hono();
 
-//TODO: Check env variables missing, throw error if missing
 health.get("/health", (c) => c.json({ status: "operational" }));
 
 export default health;
